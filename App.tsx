c4ce37ad52af9c56a9442f17fa5ce22fe990f43e
prozpekt19 <jishnudd@gmail.com>
2023-10-07 21:47:15 +0530

    second commit

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import  React,{useRef} from 'react';
+import  React,{useRef,useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   Image,
@@ -9,8 +9,10 @@ import {
   Text,
   useColorScheme,
   View,
+  TextInput,
   Animated,
   Button,
+  ScrollViewBase,
 } from 'react-native';
 
 import {
@@ -23,8 +25,7 @@ import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 import LinearGradient from 'react-native-linear-gradient';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TouchableOpacity } from 'react-native';
-import { ButtonGroup, withTheme } from '@rneui/themed';
-
+import { ButtonGroup, SearchBar, withTheme } from '@rneui/themed';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {MagicSheetPortal} from 'react-native-magic-sheet';
@@ -32,6 +33,8 @@ import { Dimensions,FlatList} from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import RNFadedScrollView from 'rn-faded-scrollview';
 import { BackgroundImage } from '@rneui/base';
+import { screensEnabled } from 'react-native-screens';
+import { SearchBarAndroid } from '@rneui/base/dist/SearchBar/SearchBar-android';
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -69,6 +72,17 @@ function App(): JSX.Element {
   const backgroundStyle = {
     backgroundColor: Colors.black,
   };
+  const [searchQuery, setSearchQuery] = useState('');
+  // Your data to be filtered goes here, replace this with your actual data
+  const data = [
+    { title: 'Overview' },
+    { title: 'Mineral' },
+    { title: 'm'}
+    // ... more data items
+  ];
+  const filteredData = data.filter((item) =>
+  item.title.toLowerCase().includes(searchQuery.toLowerCase())
+);
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -81,20 +95,39 @@ function App(): JSX.Element {
            size={25}
            color="white"
           />
-          <AntDesignIcon 
+          <AntDesignIcon
            style={styles.search}
            name="menufold"
            size={25}
            color="white"
           />
-          <Section title="EMIT">
+            <Section title="EMIT">
           </Section>
+           <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search ..."
+          placeholderTextColor={'black'}
+          onChangeText={(text) => setSearchQuery(text)}
+          value={searchQuery}
+        />
+      </View>
+      
+      {/* Display filtered data */}
+      <FlatList
+        data={filteredData}
+        renderItem={({ item }) => (
+          <Text style={styles.sectionTitle}>{item.title}</Text>
+        )}
+        keyExtractor={(item) => item.title}
+      />
       <ScrollView fadingEdgeLength={60}
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle} 
         decelerationRate={'normal'}
         alwaysBounceVertical
-        > 
+        disableIntervalMomentum
+        >
         <View
           style={{
             backgroundColor:Colors.black,
@@ -125,13 +158,13 @@ function App(): JSX.Element {
            color="black"
           />
           <ScrollView
-          horizontal={true}
           pagingEnabled
-          showsHorizontalScrollIndicator={false}
+          showsHorizontalScrollIndicator={true}
           decelerationRate={'fast'}
-          contentInsetAdjustmentBehavior="automatic"  
+          contentInsetAdjustmentBehavior="automatic"
+          horizontal={true}  
         >
-         <LinearGradient start={{x: 0, y: 0}} end={{x: 0, y: 1}} colors={['#cc9900','#663300', 'grey']} style={styles.linearGradient}>
+         <LinearGradient start={{x: 0, y: 0}} end={{x: 0, y: 1}} colors={['teal','red', 'grey']} style={styles.linearGradient}>
          <Text style={styles.info4}>
           GET YOUR FACTS RIGHT!
          </Text>
@@ -140,21 +173,37 @@ function App(): JSX.Element {
           source={require("./assets/cards.png")}
           />
          </LinearGradient>
-         <LinearGradient start={{x: 1, y: 0}} end={{x: 1, y: 1}} colors={['blue','violet', 'grey']} style={styles.linearGradient}>
+         <LinearGradient start={{x: 0, y: 1}} end={{x: 1, y: 0}} colors={['blue','violet', 'grey']} style={styles.linearGradient}>
          <Image
          style={styles.cards}
          source={require("./assets/cards.png")}
          />
          </LinearGradient>
+         <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['green','black','grey']} style={styles.linearGradient}>
           <Image 
           style={styles.cards}
           source={require("./assets/cards.png")}
           />
+          </LinearGradient>
           </ScrollView>
+          <Text
+          style={styles.heading}>
+            OVERVIEW
+          </Text>
+          < AntDesignIcon
+           style={styles.oicon}
+           name="codepen"
+           size={30}
+           color='black'
+          />
      <Image 
        style={styles.imagestyle2}
        source={require("./assets/emit1.png")}
        />
+       <Text 
+       style={styles.info3}>
+        THE EMIT BEFORE DEPLOYMENT
+       </Text>
        <Text
        style={styles.info}>
        Mineral dust blown into the air is an important part of the Earth system.  Wind blows dust into the atmosphere from desert regions around the world, and can carry dust across oceans. Dark minerals that absorb sunlight can warm the Earth, while light-colored minerals can cool the Earth. By accurately mapping the composition of areas that produce mineral dust, EMIT will advance our understanding of dust’s effects throughout the Earth system and to human populations now and in the future.
@@ -165,6 +214,29 @@ function App(): JSX.Element {
        style={styles.imagestyle3}
        source={require("./assets/chart.png")}
        />
+        <Text
+       style={styles.info}>
+        The EMIT mission will image the mineral content of dust producing regions of the Earth. As part of this effort, it will collect unprecedented coverage of visible to shortwave infrared (VSWIR) radiance and reflectance data and use them to produce estimates of the fraction of the surface covered by green vegetation, non-photosynthesizing vegetation, and ten important minerals over arid regions of the planet
+       </Text>
+       <Text
+       style={styles.heading1}>
+      ACTIVE APPLICATIONS AREA
+       </Text> 
+       < AntDesignIcon
+           style={styles.oicon}
+           name="checkcircle"
+           size={30}
+           color='black'
+          />
+        <Text
+       style={styles.subheading}>
+      GREEN HOUSE GAS MAPPING {'\n\n'}
+      Carbon dioxide and methane are the two dominant gasses that contribute to climate change.{'\n\n'}Methane traps more heat in the atmosphere per molecule than carbon dioxide, therefore targeting reductions in anthropogenic methane emissions offers an effective approach to decrease overall atmospheric warming.{'\n\n'}Methane and carbon dioxide have distinct absorption features in the shortwave infrared (1800-2500nm) that permit the detection and mapping of point source greenhouse gas emissions with EMIT data.{'\n\n'}Identifying strong emission sources offers the potential to improve our understanding of anthropogenic emissions and to mitigate those emissions.
+       </Text>
+       <Text
+       style={styles.heading}>
+      FUTURE APPLICATIONS
+       </Text>
        </View>
        </ScrollView>
     </SafeAreaView>
@@ -182,6 +254,8 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     marginLeft:130,
     marginTop:-65,
+    paddingTop:0,
+    marginBottom:0,
   },
   sectionDescription: {
     fontSize:12,
@@ -217,6 +291,16 @@ const styles = StyleSheet.create({
     paddingBottom:90,
     paddingTop:-200,
   },
+  subheading:{
+    fontSize:14,
+    fontFamily:'JetBrainsMono-Regular',
+    paddingBottom:10,
+    color:'white',
+    paddingTop:0,
+    marginBottom:10,
+    marginTop:-20,
+    marginLeft:17,
+  },
   imagestyle:{
     width:200,
     height:200,
@@ -237,7 +321,28 @@ const styles = StyleSheet.create({
     paddingLeft:10,
     paddingRight:10,
     marginBottom:80,
-    color:'silver',
+    color:'white',
+  },
+  info3:{
+    fontFamily:'roboto.bold',
+    fontSize:16,
+    paddingLeft:70,
+    paddingRight:10,
+    marginBottom:80,
+    color:'white',
+  },
+  searchContainer:{
+    backgroundColor:'white',
+    borderRadius:30,
+    paddingLeft:15,
+    marginLeft:10,
+    marginRight:20,
+    marginTop:0,
+    marginBottom:15,
+  },
+  searchInput:{
+   color:'black',
+   fontFamily:'JetBrainsMono-Bold',
   },
   info1:{
     backgroundColor:'white',
@@ -252,14 +357,20 @@ const styles = StyleSheet.create({
     marginRight:40,
     color:'black',
     borderRadius:30,
-    marginTop:40,
+  },
+  oicon:{
+      marginLeft:305,
+      marginTop:-200,
+      marginBottom:100,
+      paddingBottom:90,
+      paddingTop:-200,
   },
   imagestyle2:{
-    width:200,
+    width:300,
     height:200,
-    marginBottom:70,
-    marginTop:-10,
-    marginLeft:92,
+    marginBottom:50,
+    marginTop:-70,
+    marginLeft:45,
     borderRadius:18,
   },
   imagestyle3:{
@@ -275,22 +386,26 @@ const styles = StyleSheet.create({
     paddingRight:1,
     borderRadius: 20,
     marginBottom:110,
-    marginLeft:7,
-    marginRight:0,
+    marginLeft:9,
+    marginRight:4,
     paddingBottom:50,
     width:372,
-    alignSelf:'auto'
+    alignSelf:'auto',
+    alignItems:'flex-start',
+    marginEnd:0,
   },
   cards:{
     width:360,
     height:310,
     paddingBottom:200,
-    marginBottom:200,
+    marginBottom:80,
     borderRadius:20,
-    marginLeft:5,
-    marginRight:17,
-    marginTop:50,
-    paddingTop:10,
+    marginLeft:4,
+    marginRight:0,
+    marginTop:52,
+    paddingTop:0,
+    paddingLeft:10,
+    paddingRight:10,
   },
   info4:{
     backgroundColor:'white',
@@ -305,7 +420,7 @@ const styles = StyleSheet.create({
     marginRight:40,
     color:'black',
     borderRadius:13,
-    marginTop:20,
+    marginTop:30,
   },
   linearGradient1:{
       marginLeft:330,
@@ -321,6 +436,38 @@ const styles = StyleSheet.create({
     margin: 10,
     color: 'white',
     backgroundColor: 'transparent',
+  },
+  heading:{
+    backgroundColor:'white',
+    fontFamily:'JetBrainsMono-Bold',
+    fontSize:19,
+    paddingLeft:55,
+    paddingRight:10,
+    marginBottom:150,
+    paddingTop:20,
+    paddingBottom:25,
+    marginLeft:0,
+    marginRight:40,
+    color:'black',
+    borderTopRightRadius:20,
+    borderBottomRightRadius:20,
+    marginTop:20,
+  },
+  heading1:{
+    backgroundColor:'white',
+    fontFamily:'JetBrainsMono-Bold',
+    fontSize:17,
+    paddingLeft:30,
+    paddingRight:10,
+    marginBottom:150,
+    paddingTop:20,
+    paddingBottom:25,
+    marginLeft:0,
+    marginRight:40,
+    color:'black',
+    borderTopRightRadius:20,
+    borderBottomRightRadius:20,
+    marginTop:20,
   },
   buttonsContainer: {
     flexDirection: 'row',
